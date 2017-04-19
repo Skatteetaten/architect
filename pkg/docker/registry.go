3c6e471af122443e28857ba4201353bd0dfe280d
Eriksen, Øyvind <Oyvind.Eriksen2@skatteetaten.no>
2017-04-19 15:14:12 +0200

    AOS-1172 First version of registry client

---
+++ pkg/docker/registry.go
@@ -0,0 +1,134 @@
+package docker
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/docker/distribution/manifest/schema1"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+type RegistryClient interface {
+	PullManifest(repository string, tag string) ([]byte, error)
+}
+
+type HttpClient struct {
+	address string
+}
+
+func NewHttpClient(address string) *HttpClient {
+	return &HttpClient{address: address}
+}
+
+func (registry *HttpClient) PullManifest(repository string, tag string) ([]byte, error) {
+	url := fmt.Sprintf("%s/v2/%s/manifests/%s", registry.address, repository, tag)
+
+	res, err := http.Get(url)
+
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer res.Body.Close()
+
+	return body, nil
+}
+
+func GetManifestEnv(client RegistryClient, repository string, tag string, name string) (string, error) {
+
+	body, err := client.PullManifest(repository, tag)
+
+	manifest := &schema1.SignedManifest{}
+
+	if err = manifest.UnmarshalJSON(body); err != nil {
+		return "", fmt.Errorf("Failed to unmarshal manifest for image %s, tag %s from Docker registry: %v", repository, tag, err)
+	}
+
+	if err != nil {
+		return "", err
+	}
+
+	value, err := getEnvFromV1Data(manifest.History[0].V1Compatibility, name)
+
+	if err != nil {
+		return "", fmt.Errorf("Failed to extract env variable %s from manifest for image %s, tag %s: %v", name, repository, tag, err)
+	}
+
+	return value, nil
+}
+
+func getEnvFromV1Data(v1data string, name string) (string, error) {
+
+	m := map[string]interface{}{}
+
+	if err := json.Unmarshal([]byte(v1data), &m); err != nil {
+		return "", err
+	}
+
+	configMap, err := getConfigMap(m)
+
+	if err != nil {
+		return "", err
+	}
+
+	envArray, err := getEnvArray(configMap)
+
+	if err != nil {
+		return "", err
+	}
+
+	for _, entry := range envArray {
+		pair, ok := entry.(string)
+
+		if !ok {
+			return "", fmt.Errorf("Failed to read variable")
+		}
+
+		s := strings.Split(pair, "=")
+
+		if len(s) != 2 {
+			continue
+		}
+
+		if s[0] == name {
+			return s[1], nil
+		}
+	}
+
+	return "", nil
+}
+
+func getConfigMap(m map[string]interface{}) (map[string]interface{}, error) {
+	if m["config"] == nil {
+		return nil, fmt.Errorf("Missing \"config\" object")
+	}
+
+	config, ok := m["config"].(map[string]interface{})
+
+	if !ok {
+		return nil, fmt.Errorf("Object \"config\" has unexpected type")
+	}
+
+	return config, nil
+}
+
+func getEnvArray(m map[string]interface{}) ([]interface{}, error) {
+	if m["Env"] == nil {
+		return nil, fmt.Errorf("Missing \"Env\" object")
+	}
+
+	env, ok := m["Env"].([]interface{})
+
+	if !ok {
+		return nil, fmt.Errorf("Object \"Env\" has unexpected type")
+	}
+
+	return env, nil
+}
---
+++ pkg/docker/registry_test.go
@@ -0,0 +1,44 @@
+package docker
+
+import (
+	"fmt"
+	"io/ioutil"
+	"testing"
+)
+
+const expected_value = "1.7.0"
+
+type RegistryClientMock struct {
+}
+
+func (registry *RegistryClientMock) PullManifest(repository string, tag string) ([]byte, error) {
+	return testmanifest()
+}
+
+func TestGetManifestEnv(t *testing.T) {
+
+	// Uncomment to pull manifest from registry:
+	//rc := NewHttpClient("http://uil0map-paas-app01:9090")
+
+	rc := &RegistryClientMock{}
+
+	actual, err := GetManifestEnv(rc, "aurora/oracle8", "1", "BASE_IMAGE_VERSION")
+
+	if err != nil {
+		fmt.Println("ERROR: ", err)
+	}
+
+	if actual != expected_value {
+		t.Error("Expected", expected_value, "got", actual)
+	}
+}
+
+func testmanifest() ([]byte, error) {
+	buf, err := ioutil.ReadFile("testdata/manifest.json")
+
+	if err != nil {
+		return nil, err
+	}
+
+	return buf, nil
+}
