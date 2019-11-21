08b5d094f8d46837d24f18b758eec087aedaa70d
Rune Offerdal <Rune.Offerdal@skatteetaten.no>
2019-11-21 12:19:30 +0100

    SITJ-669 fixing gomft after rebase against last master release

--- pkg/nodejs/prepare/types.go
+++ pkg/nodejs/prepare/types.go
@@ -5,23 +5,23 @@ import "github.com/skatteetaten/architect/pkg/config/runtime"
 type auroraApplication struct {
 	NodeJS *nodeJSApplication `json:"nodejs"`
 	//Deprecated
-	Static            string               `json:"static"`
-	Webapp            *webApplication      `json:"webapp"`
-	ConfigurableProxy bool            	   `json:"configurableProxy"`
-	Gzip            nginxGzip              `json:"gzip"`
-	Locations       map[string]interface{} `json:"locations"`
-	Exclude         []string               `json:"exclude"`
+	Static            string                 `json:"static"`
+	Webapp            *webApplication        `json:"webapp"`
+	ConfigurableProxy bool                   `json:"configurableProxy"`
+	Gzip              nginxGzip              `json:"gzip"`
+	Locations         map[string]interface{} `json:"locations"`
+	Exclude           []string               `json:"exclude"`
 	//Deprecated
-	Path string                            `json:"path"`
+	Path string `json:"path"`
 	//Deprecated
-	SPA bool                               `json:"spa"`
+	SPA bool `json:"spa"`
 }
 
 type webApplication struct {
-	StaticContent   string                 `json:"content"`
-	Path            string                 `json:"path"`
-	Headers         map[string]string      `json:"headers"`	
-	DisableTryfiles bool                   `json:"disableTryfiles"`
+	StaticContent   string            `json:"content"`
+	Path            string            `json:"path"`
+	Headers         map[string]string `json:"headers"`
+	DisableTryfiles bool              `json:"disableTryfiles"`
 }
 
 type nodeJSApplication struct {
