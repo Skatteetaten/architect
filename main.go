be1876cb9acb1140dea4b534aa5fe01b3d81bb0d
Bjørn Åge Tungesvik <BjornAge.Tungesvik@skatteetaten.no>
2020-02-26 09:28:51 +0100

    AOT-831 architect skal ikke renames i container

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ import (
 
 func main() {
 	// We are called main. Assume we run in a container
-	if strings.HasSuffix(os.Args[:1][0], "main") && len(os.Args) == 1 {
+	if strings.HasSuffix(os.Args[:1][0], "architect") && len(os.Args) == 1 {
 		initializeAndRunOnOpenShift()
 	} else {
 		cmd.Execute()
