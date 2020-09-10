43f4ee322f8cd519f1f851b763c7ecdfb71cee24
Bjørn Åge Tungesvik <BjornAge.Tungesvik@skatteetaten.no>
2020-09-10 07:35:27 +0200

    AOT-999 Clean java prepare

--- pkg/java/build.go
+++ pkg/java/build.go
@@ -8,14 +8,11 @@ import (
 	"github.com/skatteetaten/architect/pkg/java/prepare"
 	"github.com/skatteetaten/architect/pkg/nexus"
 	"github.com/skatteetaten/architect/pkg/process/build"
-	"strings"
 )
 
 func Prepper() process.Prepper {
 	return func(cfg *config.Config, auroraVersion *runtime.AuroraVersion, deliverable nexus.Deliverable,
 		baseImage runtime.BaseImage) ([]docker.DockerBuildConfig, error) {
-
-		if strings.ToLower(cfg.BuildStrategy) == config.Layer {
 			buildConfiguration, err := prepare.PrepareLayers(cfg.DockerSpec, auroraVersion, deliverable, baseImage)
 			if err != nil {
 				return nil, errors.Wrap(err, "Error while preparing layers")
@@ -29,18 +26,5 @@ func Prepper() process.Prepper {
 				Labels:           buildConfiguration.Labels,
 				Cmd:              buildConfiguration.Cmd,
 			}}, nil
-
-		} else {
-			buildPath, err := prepare.Prepare(cfg.DockerSpec, auroraVersion, deliverable, baseImage)
-			if err != nil {
-				return nil, errors.Wrap(err, "Error while preparing the docker context")
-			}
-			return []docker.DockerBuildConfig{{
-				AuroraVersion:    auroraVersion,
-				BuildFolder:      buildPath,
-				DockerRepository: cfg.DockerSpec.OutputRepository,
-				Baseimage:        baseImage.DockerImage,
-			}}, nil
-		}
 	}
 }
--- pkg/java/prepare/prepare.go
+++ pkg/java/prepare/prepare.go
@@ -1,9 +1,7 @@
 package prepare
 
 import (
-	"fmt"
 	"github.com/pkg/errors"
-	"github.com/sirupsen/logrus"
 	"github.com/skatteetaten/architect/pkg/config"
 	"github.com/skatteetaten/architect/pkg/config/runtime"
 	"github.com/skatteetaten/architect/pkg/docker"
@@ -26,59 +24,6 @@ type BuildConfiguration struct {
 	Env          map[string]string
 	Labels       map[string]string
 	Cmd          []string
-}
-
-func Prepare(dockerSpec config.DockerSpec, auroraVersions *runtime.AuroraVersion, deliverable nexus.Deliverable, baseImage runtime.BaseImage) (string, error) {
-	// Create docker build folder
-	dockerBuildPath, err := ioutil.TempDir("", "deliverable")
-
-	if err != nil {
-		return "", errors.Wrap(err, "Failed to create root folder of Docker context")
-	}
-
-	// Unzip deliverable
-	applicationFolder := filepath.Join(dockerBuildPath, util.ApplicationBuildFolder)
-	err = util.ExtractAndRenameDeliverable(dockerBuildPath, deliverable.Path)
-
-	if err != nil {
-		return "", errors.Wrap(err, "Failed to extract application archive")
-	}
-
-	// Load metadata
-	meta, err := loadDeliverableMetadata(filepath.Join(applicationFolder, util.DeliveryMetadataPath))
-
-	if err != nil {
-		return "", errors.Wrap(err, "Failed to read application metadata")
-	}
-
-	fileWriter := util.NewFileWriter(dockerBuildPath)
-
-	if architecture, exists := baseImage.ImageInfo.Labels["www.skatteetaten.no-imageArchitecture"]; exists && architecture == "java" {
-
-		logrus.Info("Running radish build")
-		if err := fileWriter(newRadishDescriptor(meta, filepath.Join(util.DockerBasedir, util.ApplicationFolder)), "radish.json"); err != nil {
-			return "", errors.Wrap(err, "Unable to create radish descriptor")
-		}
-		if err = fileWriter(NewRadishDockerFile(dockerSpec, *auroraVersions, *meta, baseImage.DockerImage, docker.GetUtcTimestamp()),
-			"Dockerfile"); err != nil {
-			return "", errors.Wrap(err, "Failed to create Dockerfile")
-		}
-		//TODO: Hack. Remove code later
-	} else if architecture, exists := baseImage.ImageInfo.Labels["www.skatteetaten.no-imageArchitecture"]; exists && architecture == "java-test" {
-		logrus.Info("Running test image build")
-
-		if err := fileWriter(newRadishDescriptor(meta, filepath.Join(util.DockerBasedir, util.ApplicationFolder)), "radish.json"); err != nil {
-			return "", errors.Wrap(err, "Unable to create radish descriptor")
-		}
-		if err = fileWriter(NewRadishTestImageDockerFile(dockerSpec, *auroraVersions, *meta, baseImage.DockerImage, docker.GetUtcTimestamp()),
-			"Dockerfile"); err != nil {
-			return "", errors.Wrap(err, "Failed to create Dockerfile")
-		}
-	} else {
-		return "", fmt.Errorf("The base image provided does not support radish. Make sure you use the latest version")
-	}
-
-	return dockerBuildPath, nil
 }
 
 func PrepareLayers(dockerSpec config.DockerSpec, auroraVersions *runtime.AuroraVersion, deliverable nexus.Deliverable, baseImage runtime.BaseImage) (*BuildConfiguration, error) {
